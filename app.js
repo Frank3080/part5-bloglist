5f7c0cddb291a95fc67da1f0562a44d8d9203c7e
Frank3080 <147672388+Frank3080@users.noreply.github.com>
2024-01-03 06:49:44 +0100

    update commit

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const config = require("./utils/config");
 const logger = require("./utils/logger");
 const blogRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users");
 
 const mongoUrl = config.MONGODB_URI;
 mongoose
@@ -19,5 +20,6 @@ mongoose
 app.use(cors());
 app.use(express.json());
 app.use("/api/blogs", blogRouter);
+app.use("/api/users", usersRouter);
 
 module.exports = { app };
