36a8e097d6b86cd5a5e53260bbd5e7ea8d9525b2
Frank3080 <147672388+Frank3080@users.noreply.github.com>
2024-01-12 16:47:29 +0100

    update commit

--- bloglist-frontend/src/components/Toggable.jsx
+++ bloglist-frontend/src/components/Toggable.jsx
@@ -1,14 +1,18 @@
-import { useState } from "react";
+import { useState, forwardRef, useImperativeHandle } from "react";
+import PropTypes from "prop-types";
 
-const Togglable = (props) => {
+const Togglable = forwardRef((props, ref) => {
   const [visible, setVisible] = useState(false);
 
   const hideWhenVisible = { display: visible ? "none" : "" };
   const showWhenVisible = { display: visible ? "" : "none" };
 
-  const toggleVisibility = () => {
-    setVisible(!visible);
-  };
+  const toggleVisibility = () => setVisible(!visible);
+  useImperativeHandle(ref, () => {
+    return {
+      toggleVisibility,
+    };
+  });
 
   return (
     <div>
@@ -21,6 +25,10 @@ const Togglable = (props) => {
       </div>
     </div>
   );
+});
+
+Togglable.propTypes = {
+  buttonLabel: PropTypes.string.isRequired,
 };
 
 export default Togglable;
